5bc91fe718a5124d5a00ca78f915255ca13f1987
Swizec Teller <swizec@swizec.com>
2018-09-06 23:57:12 -0700

    Update Datapoint.js

--- src/Datapoint.js
+++ src/Datapoint.js
@@ -5,20 +5,7 @@ import chroma from "chroma-js";
 import DashboardContext from "./DashboardContext";
 
 const Circle = styled.circle`
-    fill: ${({ highlighted }) =>
-        highlighted
-            ? chroma("red")
-                  .brighten(2)
-                  .hex()
-            : "#008ae5"}
-    fill-opacity: 0.5;
-    stroke: ${({ highlighted }) =>
-        highlighted
-            ? chroma("red")
-                  .brighten(2)
-                  .hex()
-            : "#008ae5"}
-    transition: r .2s ease-in-out;
+
 `;
 
 class Datapoint extends React.Component {
